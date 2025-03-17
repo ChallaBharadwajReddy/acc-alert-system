e41d6b640606fab14178cea205825754f53586d4
nishanth <nishanth100000@gmail.com>
2025-03-17 23:54:35 +0530

    initial commit 1

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -6,14 +6,7 @@ import {getFirestore} from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyClcxdM87GuzeDGZPbHH8JCUutM_ThoGz4",
-  authDomain: "accident-alert-7b547.firebaseapp.com",
-  projectId: "accident-alert-7b547",
-  storageBucket: "accident-alert-7b547.firebasestorage.app",
-  messagingSenderId: "85211240209",
-  appId: "1:85211240209:web:44e35f6e1821d2e0a89eb0"
-};
+ 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
